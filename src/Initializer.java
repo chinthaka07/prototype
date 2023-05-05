0324ea6af284b41231e26f9ce1bdd5e850bd4ed7
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-05 14:56:13 +0530

    2

--- src/Initializer.java
+++ src/Initializer.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Initializer {
+public class Initializer {
+    public static void main(String[] args) {
+        ComputerShop shop1 = new ComputerShop();
+        shop1.setShop("Colombo");
+        System.out.println(shop1);
+    }
 }
